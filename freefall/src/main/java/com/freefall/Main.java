e517e23001e07d5a66c979e5af44172c00be3b03
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-11-18 16:13:56 +0200

    setting up main gui

--- freefall/src/main/java/com/freefall/Main.java
+++ freefall/src/main/java/com/freefall/Main.java
@@ -1,8 +1,14 @@
 package com.freefall;
 import javax.swing.JFrame;
+import javax.swing.JButton;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.setSize(400,640);
+        JButton dropballButton =new JButton();
+        dropballButton.setBounds(50,100,95,30);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
         //frame add 
     }
 }
