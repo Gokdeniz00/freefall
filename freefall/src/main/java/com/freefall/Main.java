67f9e53fd72e5605842f2e2f67345c756fd159f6
Gökdeniz Dağ <mountainskysea07@gmail.com>
2023-11-18 16:20:50 +0200

    added button

--- freefall/src/main/java/com/freefall/Main.java
+++ freefall/src/main/java/com/freefall/Main.java
@@ -4,11 +4,13 @@ import javax.swing.JButton;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-        frame.setSize(400,640);
-        JButton dropballButton =new JButton();
+        frame.setSize(400,400);
+        JButton dropballButton =new JButton("Drop the ball");
         dropballButton.setBounds(50,100,95,30);
+        frame.add(dropballButton);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setLayout(null);
         //frame add 
     }
 }
